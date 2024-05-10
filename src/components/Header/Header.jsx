39affcbe62417c40ab6f4dd858377b2bc23bc546
itsabdulrafey <ahmedrafey6@gmail.com>
2024-05-10 16:34:41 +0500

    Update Routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import { Heading, Img, Button, Text } from "../../components";
+import { Link } from 'react-router-dom';
 
 // Assuming Img is a styled img tag, we'll replace it with MUI's Box component for this example
 const StyledImage = styled(Box)({
@@ -25,11 +26,11 @@ const Header = () => {
           <div className="flex sm:flex-col justify-between items-center gap-5">
             <Img src="images/img_image_19.png" alt="imagenineteen" className="w-[98px] sm:w-full object-cover" />
             <div className="flex justify-between items-center w-[23%] sm:w-full gap-5">
-              <a href="#">
+              <Link to="Registeration">
                 <Heading size="lg" as="h6" className="!text-gray-900 tracking-[3.20px] uppercase">
                   Sign up
                 </Heading>
-              </a>
+              </Link>
               <Button size="sm" shape="round" className="sm:px-5 tracking-[3.20px] uppercase min-w-[150px]">
                 Log in
               </Button>
