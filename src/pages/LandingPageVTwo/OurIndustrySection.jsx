a5ee8758c987ba5a48bd1e4278a012fba4c8321c
itsabdulrafey <ahmedrafey6@gmail.com>
2024-09-18 12:31:34 +0500

    Our Industry Partners: Reorder:1) FXCM, 2) XTB, 3) LSEG, 4) Cuneus, 5) ATFX, 6) TRADU, 7) MiTRADE

--- src/pages/LandingPageVTwo/OurIndustrySection.jsx
+++ src/pages/LandingPageVTwo/OurIndustrySection.jsx
@@ -9,38 +9,44 @@ import tick from "../../assets/images/tick.png";
 import { Link, useNavigate } from "react-router-dom";
 const OurIndustrySection = () => {
   return (
-     <>
+    <>
       <div className="flex flex-col items-center w-full gap-[57px] mx-auto md:p-5 sm:gap-7 max-w-[1500px]">
-                <Heading
-                  size="4xl"
-                  as="h2"
-                  className="!text-black-900 tracking-[1.60px] uppercase"
-                >
-                  Our Industry Partners
-                </Heading>
-                <div className="flex md:flex-col self-stretch justify-evenly items-center gap-10">
-                  <Img
-                    src="images/img_image_5.png"
-                    alt="imagefive_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                  <Img
-                    src="images/img_image_6.png"
-                    alt="imagesix_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                    <Img
-                    src="images/Mitrade-forex-broker-Review-2.png"
-                    alt="imagesix_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                <Img
-  src="images/cuneus logo word (transparent).png"
-  alt="imagesix_one"
-  className="w-[22%] md:w-full object-cover"
-  />
+        <Heading
+          size="4xl"
+          as="h2"
+          className="!text-black-900 tracking-[1.60px] uppercase"
+        >
+          Our Industry Partners
+        </Heading>
+        <div className="flex md:flex-col self-stretch justify-evenly items-center gap-10">
+          <Img
+            src="images/img_image_4.png"
+            alt="imagefour_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+          <Img
+            src="images/img_image_5.png"
+            alt="imagefive_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+          <Img
+            src="images/img_image_7.png"
+            alt="imageseven_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+          <Img
+            src="images/cuneus logo word (transparent).png"
+            alt="imagesix_one"
+            className="w-[22%] md:w-full object-cover"
+          />
 
-                  {/* <Button
+          <Img
+            src="images/img_image_6.png"
+            alt="imagesix_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+
+          {/* <Button
                     color="indigo_A700"
                     size="2xl"
                     leftIcon={<Img src="images/cuneus logo word (transparent).png" alt="image 8" className="w-[42px] h-[40px]" />}
@@ -48,25 +54,20 @@ const OurIndustrySection = () => {
                   >
                     Cuneus
                   </Button> */}
-                   <Img
-                    src="images/Tradu_Logo_Suite_RGB_Black54-1536x540.png"
-                    alt="imageseven_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                  <Img
-                    src="images/img_image_7.png"
-                    alt="imageseven_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                  <Img
-                    src="images/img_image_4.png"
-                    alt="imagefour_one"
-                    className="w-[9%] md:w-full object-cover"
-                  />
-                </div>
-              </div>
-     </>
-     )
-}
+          <Img
+            src="images/Tradu_Logo_Suite_RGB_Black54-1536x540.png"
+            alt="imageseven_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+          <Img
+            src="images/Mitrade-forex-broker-Review-2.png"
+            alt="imagesix_one"
+            className="w-[9%] md:w-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default OurIndustrySection
+export default OurIndustrySection;
