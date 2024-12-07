8d797e336742599b16e6479559ff75a2e8910afd
itsabdulrafey <ahmedrafey6@gmail.com>
2024-12-08 02:24:10 +0500

    test in Inlight

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
